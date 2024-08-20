dbf557a995492f0b530fb226cb2c6c3a53b226c2
nathan <nathanlogan81@gmail.com>
2024-08-20 15:47:38 -0500

    feat: challenge 12 complete

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const session = require('express-session');
 const { ObjectID } = require('mongodb');
 const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -50,7 +51,7 @@ myDB(async client => {
       console.log(`User ${username} attempted to log in.`);
       if(err) return done(err);
       if (!user) return done(null, false);
-      if (password !== user.password) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) return done(null, false);
       return done(null, user);
     })
   }));
@@ -68,13 +69,14 @@ myDB(async client => {
   });
 
   app.route('/register').post((req, res, next) => {
+    const hash = bcrypt.hashSync(req.body.password, 12);
     myDataBase.findOne({ username: req.body.username }, (err, user) => {
       if (err) {
         next(err);
       } else if (user) {
         res.redirect('/');
       } else {
-        myDataBase.insertOne({ username: req.body.username, password: req.body.password },
+        myDataBase.insertOne({ username: req.body.username, password: hash },
           (err, doc) => {
             if (err) {
               res.redirect('/');
