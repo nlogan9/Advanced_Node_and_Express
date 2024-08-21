769bd7f8234524ac13d6988f227e1391be501b96
nathan <nathanlogan81@gmail.com>
2024-08-21 12:39:39 -0500

    feat: challenge 16 complete

--- auth.js
+++ auth.js
@@ -14,9 +14,27 @@ module.exports = function (app, myDataBase) {
   },
     function(accessToken, refreshToken, profile, cb) {
       console.log(process.env.GITHUB_CLIENT_ID);
-      /*UserActivation.findOrCreate({ githubID: profile.id }, function (err, user) {
-        return cb(err, user);
-      });*/
+      myDataBase.findOneAndUpdate({ id: profile.id }, {
+        $setOnInsert: {
+          id: profile.id,
+          username: profile.username,
+          name: profile.displayname || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails) ? profile.emails[0].value : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || ''
+        },
+        $set: {
+          last_login: new Date()
+        },
+        $inc: {
+          login_count: 1
+        }
+      },
+      { upsert: true, new: true }, (err, doc) => {
+        return cb(null, doc.value);
+      }
+    )
     }
 ));
     
--- routes.js
+++ routes.js
@@ -54,7 +54,7 @@ module.exports = function (app, myDataBase) {
   app.route('/auth/github').get(passport.authenticate('github'));
 
   app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
-      res.text;
+      res.redirect('/profile');
     });
 
   app.use((req, res, next) => {
